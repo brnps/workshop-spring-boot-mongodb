d17ceffe90a2e447c07c7b7f01e7d5c1c6c7293a
Bruno Pinheiro da Silva <bruno.pinheiro@ituran.com.br>
2022-11-21 22:03:59 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/cursoudemy/workshopmongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.cursoudemy.workshopmongo.services;
+
+import com.cursoudemy.workshopmongo.domain.User;
+import com.cursoudemy.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+}
