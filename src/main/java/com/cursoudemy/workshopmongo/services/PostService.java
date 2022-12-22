0a2216b5e10324c5434a63abb43ee64613b2bdbe
Bruno Pinheiro da Silva <bruno.pinheiro@ituran.com.br>
2022-12-22 15:39:02 -0300

    Consulta com vários critérios

--- src/main/java/com/cursoudemy/workshopmongo/services/PostService.java
+++ src/main/java/com/cursoudemy/workshopmongo/services/PostService.java
@@ -9,6 +9,7 @@ import com.cursoudemy.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,5 +26,10 @@ public class PostService {
 
     public List<Post> findByTitle(String text) {
         return repo.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 *60 *60 * 1000);
+        return repo.fullSearch(text, minDate, maxDate);
     }
 }
