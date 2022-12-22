c0cb7bc8c33ce1baa1388424fee4fb6a2b75d5e1
Bruno Pinheiro da Silva <bruno.pinheiro@ituran.com.br>
2022-12-21 21:27:57 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/cursoudemy/workshopmongo/domain/Post.java
+++ src/main/java/com/cursoudemy/workshopmongo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.cursoudemy.workshopmongo.domain;
 
 import com.cursoudemy.workshopmongo.dto.AuthorDTO;
+import com.cursoudemy.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -16,6 +19,8 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post(){}
 
@@ -64,6 +69,14 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
